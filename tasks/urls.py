e2ab6fc680f3bd8ce535c1c22f349dd35d0c6c66
Tetiana Sobko <sobkotiana@gmail.com>
2025-04-01 21:48:07 +0300

    Create path and view for creating task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from tasks.views import Index
+from tasks.views import Index, TaskCreateView
 
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,20 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
 class Index(generic.ListView):
     model = Task
-    queryset = Task.objects.all().prefetch_related("tags").order_by("is_completed", "-created_at")
+    queryset = Task.objects.all().prefetch_related("tags").order_by(
+        "is_completed", "-created_at"
+    )
     template_name = "tasks/index.html"
 
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("tasks:index")
