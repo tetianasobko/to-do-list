5716e74e462855bafb90e9d576d6b2f24fb58e34
Tetiana Sobko <sobkotiana@gmail.com>
2025-04-01 23:02:07 +0300

    Make deadline optional

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,6 +7,7 @@ class TaskForm(forms.ModelForm):
     deadline = forms.DateTimeField(
         input_formats=["%Y-%m-%d %H:%M:%S"],
         widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        required=False
     )
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
